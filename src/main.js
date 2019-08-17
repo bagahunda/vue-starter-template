b20dacba0b7b148a3c1b35bc1c241dd6acbd8f46
Dmitriy <dmitriy.troy@gmail.com>
2019-08-17 19:35:06 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import store from "./state/store";
+import "./components/_globals";
 
 Vue.config.productionTip = false;
 
